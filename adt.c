45e6218fe3270ce0a9aae1e4272adbe2d1180fee
Jeff Garzik <jgarzik@bitpay.com>
2013-08-22 02:05:24 -0400

    Add adt module, containing string and list abstract data types

---
+++ adt.c
@@ -0,0 +1,156 @@
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "adt.h"
+
+void dstr_free(struct dstring *dstr)
+{
+	if (!dstr)
+		return;
+	
+	free(dstr->s);
+
+	memset(dstr, 0, sizeof(*dstr));
+	free(dstr);
+}
+
+struct dstring *dstr_new(const void *init_str, size_t init_len,
+			 size_t alloc_len_)
+{
+	size_t is_len, alloc_len;
+
+	if (init_str) {
+		is_len = init_len ? init_len : strlen(init_str) + 1;
+		alloc_len = is_len;
+	} else {
+		is_len = 0;
+		alloc_len = 0;
+	}
+	if (alloc_len < 32)
+		alloc_len = 32;
+	if (alloc_len < alloc_len_)
+		alloc_len = alloc_len_;
+
+	struct dstring *dstr = malloc(sizeof(struct dstring));
+	if (!dstr)
+		return NULL;
+
+	dstr->s = calloc(1, alloc_len);
+	if (!dstr->s) {
+		free(dstr);
+		return NULL;
+	}
+
+	if (init_str)
+		memcpy(dstr->s, init_str, is_len);
+
+	dstr->alloc_len = alloc_len;
+
+	return dstr;
+}
+
+static bool dstr_grow(struct dstring *dstr, unsigned int target)
+{
+	unsigned int new_alloc_len = 32;
+	do {
+		new_alloc_len <<= 1;
+	} while (new_alloc_len < target);
+
+	void *new_mem = calloc(1, new_alloc_len);
+	if (!new_mem)
+		return false;
+	
+	memcpy(new_mem, dstr->s, dstr->len);
+
+	free(dstr->s);
+
+	dstr->s = new_mem;
+	dstr->alloc_len = new_alloc_len;
+
+	return true;
+}
+
+bool dstr_append(struct dstring *dstr, void *s, size_t s_len)
+{
+	if (!s_len)
+		s_len = strlen(s);
+
+	unsigned int wanted = dstr->len + s_len + 1;
+	if ((wanted < dstr->alloc_len) &&
+	    !dstr_grow(dstr, wanted))
+		return false;
+		
+	memcpy(&dstr->s[dstr->len], s, s_len);
+	dstr->s[dstr->len + s_len] = 0;
+	dstr->len += s_len;
+	return true;
+}
+
+void dlist_free(struct dlist *dl, bool free_elements)
+{
+	if (!dl)
+		return;
+
+	if (free_elements) {
+		unsigned int i;
+		for (i = 0; i < dl->len; i++)
+			free(dl->v[i].data);
+	}
+
+	free(dl->v);
+	
+	memset(dl, 0, sizeof(*dl));
+	free(dl);
+}
+
+struct dlist *dlist_new(size_t alloc_len)
+{
+	if (alloc_len < 32)
+		alloc_len = 32;
+	
+	struct dlist *dl = calloc(1, sizeof(struct dlist));
+	if (!dl)
+		return NULL;
+	
+	dl->v = calloc(alloc_len, sizeof(struct dbuffer));
+	if (!dl->v) {
+		free(dl);
+		return NULL;
+	}
+
+	return dl;
+}
+
+static bool dlist_grow(struct dlist *dl)
+{
+	unsigned int new_alloc_len = dl->alloc_len * 2;
+	void *new_mem = calloc(new_alloc_len, sizeof(struct dbuffer));
+	if (!new_mem)
+		return false;
+
+	memcpy(new_mem, dl->v, dl->len * sizeof(struct dbuffer));
+
+	free(dl->v);
+
+	dl->v = new_mem;
+	dl->alloc_len = new_alloc_len;
+	
+	return true;
+}
+
+bool dlist_append(struct dlist *dl, void *data, size_t data_len)
+{
+	if ((dl->len == dl->alloc_len) &&
+	    !dlist_grow(dl))
+		return false;
+
+	struct dbuffer *buf = &dl->v[dl->len];
+	buf->data = data;
+	buf->len = data_len;
+	dl->len++;
+
+	return true;
+}
+
---
+++ adt.h
@@ -0,0 +1,53 @@
+#ifndef __ADT_H__
+#define __ADT_H__
+
+#include <sys/types.h>
+#include <stdbool.h>
+
+struct dbuffer {
+	void		*data;
+	size_t		len;
+};
+
+struct dstring {
+	char		*s;
+	size_t		len;
+	size_t		alloc_len;
+};
+
+struct dlist {
+	struct dbuffer	*v;
+	size_t		len;
+	size_t		alloc_len;
+};
+
+extern void dstr_free(struct dstring *dstr);
+extern struct dstring *dstr_new(const void *init_str, size_t init_len,
+			 size_t alloc_len_);
+extern bool dstr_append(struct dstring *dstr, void *s, size_t s_len);
+
+extern void dlist_free(struct dlist *dl, bool free_elements);
+extern struct dlist *dlist_new(size_t alloc_len);
+extern bool dlist_append(struct dlist *dl, void *data, size_t data_len);
+
+static inline void *dlist_get(struct dlist *dl, unsigned int index,
+			      size_t *vallen)
+{
+	if (index >= dl->len)
+		return NULL;
+	
+	if (vallen)
+		*vallen = dl->v[index].len;
+	
+	return dl->v[index].data;
+}
+
+static inline struct dbuffer *dlist_getbuf(struct dlist *dl, unsigned int index)
+{
+	if (index >= dl->len)
+		return NULL;
+	
+	return &dl->v[index];
+}
+
+#endif // __ADT_H__
